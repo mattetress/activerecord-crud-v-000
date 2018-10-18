67c9d71ffd3e64f8d1857f752656eee814005f87
Matt Etress <mattetress@me.com>
2018-10-18 21:04:48 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -39,11 +39,11 @@ def can_get_the_first_item_in_the_database
 end
 
 def can_get_the_last_item_in_the_database
-  Movie.all.last 
+  Movie.all.last
 end
 
 def can_get_size_of_the_database
-  __
+  Movie.all.size
 end
 
 def can_find_the_first_item_from_the_database_using_id
